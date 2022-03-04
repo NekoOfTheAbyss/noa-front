472db617df81f0f2e6ad5f7c38ac28d27bba2a66
Caligo <schattenetcaligo@gmail.com>
2022-03-04 11:40:13 +0530

    allow external links

--- components/Main/Home/Card.vue
+++ components/Main/Home/Card.vue
@@ -18,7 +18,7 @@
         hover:-translate-y-1 hover:scale-110
       "
     >
-      <NuxtLink :to="link" class="md:flex p-4">
+      <NuxtLink v-if = "link.startsWith('/')" :to="link" class="md:flex p-4">
         <div class="md:flex-shrink-0">
           <img
             :src="image"
@@ -35,6 +35,23 @@
           </div>
         </font>
       </NuxtLink>
+      <a v-else :href="link" class="md:flex p-4">
+        <div class="md:flex-shrink-0">
+          <img
+            :src="image"
+            class="w-full md:h-48 md:w-48 object-cover rounded-full"
+            alt="Abyss"
+          />
+        </div>
+        <font class="flex-grow p-2 px-8">
+          <p class="text-lg font-bold pt-3 text-white">
+            {{ title }}
+          </p>
+          <div class="py-3 text-white">
+            {{ description }}
+          </div>
+        </font>
+      </a>
     </div>
   </div>
 </template>
